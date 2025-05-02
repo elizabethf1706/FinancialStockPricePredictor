42fd07bc9a204de972c8abd50f418dd50272d47d
elizabethf1706 <148167491+elizabethf1706@users.noreply.github.com>
2025-05-01 17:21:18 -0700

    Update predict_stock_groq.py
    REMOVES KEY FROM REPO and now uses env file for key

--- predict_stock_groq.py
+++ predict_stock_groq.py
@@ -1,9 +1,9 @@
 import os
 from groq import Groq
 
-def predictStockPrice(stock_keyword):
+def predictStockPrice(GROQ_API_KEY, stock_keyword):
     # Create the Groq client
-    client = Groq(api_key="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", )
+    client = Groq(api_key=GROQ_API_KEY, )
     response = client.chat.completions.create(
         model="llama3-70b-8192",
         messages=[{"role": "user", "content": f"Give me today's date. Predict the Stock Price for {stock_keyword} for tomorrow."}],
@@ -11,4 +11,4 @@ def predictStockPrice(stock_keyword):
         temperature=0.1 # this affects the AI's creativity to generate responses where 0 is only factual based responses.
     )   
 
-    return response.choices[0].message.content
+    return response.choices[0].message.contentAC
