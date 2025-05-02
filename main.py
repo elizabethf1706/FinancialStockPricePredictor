542c2c18a182cea83310ad57e4f097e48cb6a58c
elizabethf1706 <148167491+elizabethf1706@users.noreply.github.com>
2025-05-01 17:10:37 -0700

    Update main.py
    adds the stock analysis to end of doc

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ from dotenv import load_dotenv
 from sentiment_analyzer import get_sentiment_analysis   
 from sentiment_visualizer import plot_sentiment_distribution 
 from news_word_cloud import get_wordcloud
+from predict_stock_groq import predictStockPrice
 load_dotenv()
 NEWS_API_KEY = os.getenv('NEWS_API_KEY')
 
@@ -31,11 +32,11 @@ if st.button("Analyze Sentiment"):
         with st.spinner('Fetching and analyzing news...'):
             analysis_results = get_sentiment_analysis(NEWS_API_KEY, stock_keyword)
             wordcloud_results = get_wordcloud(NEWS_API_KEY, stock_keyword)
-
+            stock_prediction = predictStockPrice(stock_keyword)
 
         # if we got results back
         if analysis_results:
-            st.success("Analysis complete!")
+            st.success("Sentiment Analysis complete!")
 
             # now we just display them
             st.subheader("Analysis Results:")
@@ -45,7 +46,7 @@ if st.button("Analyze Sentiment"):
             col2.metric("Negative Articles", f"{analysis_results['negative_count']}", f"{analysis_results['negative_pct']:.1f}%")
             col3.metric("Neutral Articles", f"{analysis_results['neutral_count']}", f"{analysis_results['neutral_pct']:.1f}%")
 
-    
+
             st.write(f"*(Based on {analysis_results['total_articles']} articles from the last 30 days)*")
 
             # the bar char
@@ -60,10 +61,19 @@ if st.button("Analyze Sentiment"):
         else:
             # maybe no articles or API error stuff
             st.error(f"Couldn't get or analyze news for '{stock_keyword}'. Maybe check the ticker or try again later?") 
+        # checks to make sure word cloud results is returned then outputs them
         if wordcloud_results:
-            st.success("Analysis complete!")
-            st.pyplot(wordcloud_results)  
+            st.success("Wordcloud complete!")
+            st.pyplot(wordcloud_results) 
+            
+        # if wordcloud isnt made returns error 
         else:
             st.error(f"Couldn't make wordcloud for '{stock_keyword}'. Maybe check the ticker or try again later?") 
-        # generate word cloud
+        
+         # checks to make sure stockd results is returned then outputs them
+        if stock_prediction:
+            st.success("Here is the predicted analysis: ")
+            st.write(stock_prediction)  
+        else:
+            st.error(f"Couldn't make stock analysis for '{stock_keyword}'. Maybe check the ticker or try again later?") 
        
