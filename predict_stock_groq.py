66be5c1b61596ce4ceafbda780a98ed0b144bb87
elizabethf1706 <148167491+elizabethf1706@users.noreply.github.com>
2025-05-01 17:42:10 -0700

    Update predict_stock_groq.py

--- predict_stock_groq.py
+++ predict_stock_groq.py
@@ -3,7 +3,7 @@ from groq import Groq
 
 def predictStockPrice(GROQ_API_KEY, stock_keyword):
     # Create the Groq client
-    client = Groq(api_key=GROQ_API_KEY, )
+    client = Groq(api_key=GROQ_API_KEY)
     response = client.chat.completions.create(
         model="llama3-70b-8192",
         messages=[{"role": "user", "content": f"Give me today's date. Predict the Stock Price for {stock_keyword} for tomorrow."}],
@@ -11,4 +11,4 @@ def predictStockPrice(GROQ_API_KEY, stock_keyword):
         temperature=0.1 # this affects the AI's creativity to generate responses where 0 is only factual based responses.
     )   
 
-    return response.choices[0].message.contentAC
+    return response.choices[0].message.content
