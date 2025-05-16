23b04cd072d4badef36041c5a114297db35642d1
chrisozzz <osgoodchris94@gmail.com>
2025-05-15 17:11:04 -0700

    Added to Main, and works. ALL GOOD, now just need to work on adding new stocks.

--- analysis_groq.py
+++ analysis_groq.py
@@ -24,17 +24,25 @@ def predictStockPrice(GROQ_API_KEY, stock_keyword, financial_results,sentiment_a
 
 def advise_earnings_from_query(GROQ_API_KEY, stock_keyword, query_results, user_question):
     client = Groq(api_key=GROQ_API_KEY)
-    
+    documents = query_results["documents"][0]
+    speaker_title = query_results["metadatas"][0]
+
     content = ""
-    for entry in query_results:
-        content += entry["content"] + "\n"
+    
+    for passage in range(len(documents)):
+        entry = f"{speaker_title[passage]["speaker"]} ({speaker_title[passage]["title"]}): \n {documents[passage]} \n\n"
+        content += entry
     
     response = client.chat.completions.create(
         model = "llama3-70b-8192",
         messages=[{
             "role": "user",
-            "content": f"Based on the following excerpts from {stock_keyword}'s recent earnings call transcript, answer the user's question: 'f{user_question}'."
-        }]
+            "content": f"""Using only the following excerpts from {stock_keyword}'s most recent earnings call transcript, 
+                        provide a direct and well-supported answer to the user’s question: '{user_question}'. Base your response 
+                        strictly on the information in the excerpts. Here are the excerpts: {content}. Whenever you refer
+                        to the provided texts, instead say the most recent earnings calls. When you can, use numbered points."""
+        }],
+        temperature = 0
     )
     
     return response.choices[0].message.content
