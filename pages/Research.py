0e2125a66d4bf82081488141720a59ae490717c0
elizabethf1706 <148167491+elizabethf1706@users.noreply.github.com>
2025-05-20 12:39:24 -0700

    Update Research.py

--- pages/Research.py
+++ pages/Research.py
@@ -8,12 +8,14 @@ st.header("🔗 View the Full Paper Online")
 st.write("""
 You can access our research paper directly via the following link:
 """)
-st.markdown("[**Click here to view the paper**](https://docs.google.com/document/d/144SSsXkz2eH5UTeEqTs1iV6tnWswcXmaWkhgZ8LZ9ns/edit?tab=t.0)", unsafe_allow_html=True)
+st.markdown("[**Click here to view the paper**](https://docs.google.com/document/d/144SSsXkz2eH5UTeEqTs1iV6tnWswcXmaWkhgZ8LZ9ns/edit?tab=t.0#heading=h.ml696l5c1og)", unsafe_allow_html=True)
 
-# Section 2: Embedded PDF Viewer
-st.header("📑 Embedded PDF Preview")
+# Section 2: Embed the Google Doc (Embedded PDF Preview)
+st.header("📑 Embedded Research Paper")
 st.write("You can also read the paper right here:")
 
-# Provide the link to the publicly hosted PDF
-pdf_url = "https://your-public-link.com/your_paper.pdf"
-st.markdown(f'<iframe src="{pdf_url}" width="100%" height="800px" type="application/pdf"></iframe>', unsafe_allow_html=True)
+# Embed the document using the published URL
+iframe_code = """
+<iframe src="https://docs.google.com/document/d/144SSsXkz2eH5UTeEqTs1iV6tnWswcXmaWkhgZ8LZ9ns/pub" width="100%" height="800px"></iframe>
+"""
+st.markdown(iframe_code, unsafe_allow_html=True)
