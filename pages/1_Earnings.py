8d42178747b49c99a93ce9196e9ce874b8ab7728
chrisozzz <osgoodchris94@gmail.com>
2025-05-20 14:26:38 -0700

    stronger exception gurantee

--- pages/1_Earnings.py
+++ pages/1_Earnings.py
@@ -1,5 +1,8 @@
 import streamlit as st
-import chromadb
+try:
+    import chromadb
+except Exception as e:
+        print("Couldn't import stuff for chromadb")
 import os
 from advise_earnings import advise_earnings_from_query
 from chroma import add_ticker_to_chroma
@@ -9,20 +12,24 @@ from chroma import add_ticker_to_chroma
 def initialize_chromadb():
     return chromadb.PersistentClient(path="./chroma")
 
-CLIENT = initialize_chromadb()
+try:
+    CLIENT = initialize_chromadb()
+except Exception as e:
+    print(e)
+    CLIENT = None
 
+st.subheader("💬 Ask Groq")
+stock = st.text_input("Ask Groq about the most recent earnings calls", "TSLA")
+stock_db = stock+"12" if len(stock) < 3 else stock 
 
-if CLIENT:
-    st.subheader(f"💬 Ask Groq")
-    stock = st.text_input("Ask Groq about the most recent earnings calls", "TSLA")
-    stock_db = stock+"12" if len(stock) < 3 else stock 
+print(f"[{os.path.basename(__file__)}]  Stock: {stock}, DB name: {stock_db}")
 
-    print(f"[{os.path.basename(__file__)}]  Stock: {stock}, DB name: {stock_db}")
-
-    user_question = st.text_input("Your question: ", "What are the latest developments?")
+user_question = st.text_input("Your question: ", "What are the latest developments?")
     
-    if st.button("Ask"):
-        with st.spinner("Asking Groq..."):
+
+if st.button("Ask"):
+    with st.spinner("Asking Groq..."):
+        if CLIENT:
             add_ticker_to_chroma(stock, stock_db, CLIENT)
             stock_collection = CLIENT.get_collection(f"{stock_db}")
             print(f"[{os.path.basename(__file__)}]  Got collection")
@@ -32,7 +39,7 @@ if CLIENT:
                 n_results=10
             )
             print(f"[{os.path.basename(__file__)}]  Got query results")
-
             groq_analysis = advise_earnings_from_query(st.secrets["GROQ_API_KEY"], stock, db_query, user_question)
-
             st.write(groq_analysis)
+        else:
+            st.error("Couldn't get the database intialized!")
